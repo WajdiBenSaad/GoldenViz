63483fbbeb31efd5a37769e2ad475a935221b459
WajdiBenSaad <wajdi.ben.saad@gmail.com>
2026-03-22 14:40:16 +0100

    improve project metadata

--- src/GoldenViz/__init__.py
+++ src/GoldenViz/__init__.py
@@ -2,4 +2,4 @@
 do you know the golden rules of a successful data vizulisation? this library helps users to ensure their data viz communicate the information as clearly and neutrally as possible, while respecting a set of Golden Rules.
 """
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
